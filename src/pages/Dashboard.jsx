0ce93b6561a72e38c3d09189a5b233b4bca01241
Dusan95z <dusan204z@gmail.com>
2023-05-09 18:14:03 +0200

    initial commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Info, Repos, User, Search, Navbar } from '../components';
+import loadingImage from '../images/preloader.gif';
+import { GithubContext } from '../context/context';
+const Dashboard = () => {
+  return (
+    <main>
+      <h2>Dashboard Page</h2>
+    </main>
+  );
+};
+
+export default Dashboard;
